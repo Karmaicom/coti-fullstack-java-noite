22dcd0eeb115f58b25585b3cbd4f8b957454b678
Karmaicom Rocha <karmaicom@gmail.com>
2026-06-08 18:25:14 -0300

    Commit inicial

---
+++ aula26/api-agentes-ia/src/main/java/br/com/cotiinformatica/apiagentesia/ApiAgentesIaApplication.java
@@ -0,0 +1,13 @@
+package br.com.cotiinformatica.apiagentesia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAgentesIaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiAgentesIaApplication.class, args);
+    }
+
+}
